c1695be7798ce72e0efe16bf27c15c48e52dcccb
Isen Naiken <isen.naiken@nexomedis.com>
2026-02-16 16:02:26 +0100

    ajout du train.sh

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ADLIS
+
+```bash
+module load python
+python3 -m venv venv
+source venv/bin/activate
+pip3 install --prefix=/work/imvia/in156281/adlis/venv -r requirements.txt
+export PYTHONPATH=/work/imvia/in156281/adlis/venv/lib/python3.9/site-packages:$PYTHONPATH
+pip3 list
+```
---
+++ train.sh
@@ -0,0 +1,13 @@
+#!/bin/ksh 
+#$ -q gpu
+#$ -o result.out
+#$ -j y
+#$ -N adlis
+cd $WORKDIR
+cd /beegfs/data/work/imvia/in156281/adlis
+source /beegfs/data/work/imvia/in156281/adlis/venv/bin/activate
+module load python
+export PYTHONPATH=/work/imvia/in156281/adlis/venv/lib/python3.9/site-packages:$PYTHONPATH
+export MPLCONFIGDIR=/work/imvia/in156281/.cache/matplotlib
+
+python train.py
