522462c7ed7a6c93188248fd2e4e95b4fc0feb16
Isen Naiken <isen.naiken@nexomedis.com>
2026-02-16 16:42:27 +0100

    modif path

--- train.sh
+++ train.sh
@@ -10,4 +10,4 @@ module load python
 export PYTHONPATH=/work/imvia/in156281/adlis/venv/lib/python3.9/site-packages:$PYTHONPATH
 export MPLCONFIGDIR=/work/imvia/in156281/.cache/matplotlib
 
-python train.py
+python Classification_resnet18/train.py
