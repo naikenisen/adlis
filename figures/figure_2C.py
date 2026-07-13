52af83313117d918d5a42c419232663de38adc1f
Isen Naiken <isen.naiken@nexomedis.com>
2026-07-13 10:57:47 +0200

    conception du scripte de test externe

---
+++ figures/figure_2C.py
@@ -0,0 +1,6 @@
+""" scripte qui doit reprendre la méthodologie utilisée par
+    app.py pour relier la détection à la classification et 
+    produire un Bland-Altman plot pour comparer les prédiction
+    des humains versus ceux du modèle en terme de % de sidéroblastes
+    sur 100 et cela sur le set de test externe fourni par Julien à dataset/test_externe.  
+ """
