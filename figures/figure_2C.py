7afa158e8a8402270e4bc951185bb47895f7039d
Isen Naiken <isen.naiken@nexomedis.com>
2026-07-20 12:52:26 +0200

    ajout d'un nouveau commentaire

--- figures/figure_2C.py
+++ figures/figure_2C.py
@@ -1,6 +1,16 @@
-""" scripte qui doit reprendre la pipeline utilisée par
-    app.py pour la détection suivie de la classification et 
-    produire un Bland-Altman plot pour comparer les prédiction
-    des humains versus ceux du modèle en terme de % de sidéroblastes
-    sur 100 et cela sur le set de test externe fourni par Julien à dataset/test_externe.  
- """
+""" 
+A) Prédiction du pourcentage de SC par patients : le scripte reprendre la pipeline utilisée par
+app/app.py, pour la détection suivie de la classification des SC et SN, sur chaques images des sous-dossiers de
+dataset/test_externe. Pour chaque sous-dossiers de dataset/test_externe il détermine un pourcentrage
+de sidéroblastes en couronnes avec la formule suivant ((SC/SC+SN)*100). 
+Le scripte crée "dataset/inference-test-externe.csv" avec deux colonnes : "id" qui correspond au sous-dossiers
+et "prediction" qui correspond au pourcentrage de sidéroblastes en couronnes.
+
+B) Création du Bland-Altman plot et régression linéaire : le scripte reprendre 
+"dataset/test-externe.csv" et "dataset/inference-test-externe.csv" pour produire 
+un Bland-Altman plot et une regression linéaire pour comparer les "prediction" de 
+"dataset/inference-test-externe.csv" aux "valeur" de "dataset/test-externe.csv". 
+La correspondance se fera entre "id" de "dataset/test-externe.csv" et "id" de 
+"dataset/inference-test-externe.csv".
+
+"""
