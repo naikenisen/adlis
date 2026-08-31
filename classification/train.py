49563487dba3fbdb7a19089d2229b8eb2ab322c5
Isen Naiken <isen.naiken@nexomedis.com>
2026-08-31 11:45:46 +0200

    switching 150 epoch

--- classification/train.py
+++ classification/train.py
@@ -19,7 +19,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Hyperparameters
 batch_size = 64
 learning_rate = 0.001
-num_epochs = 100
+num_epochs = 150
 num_classes = 2
 
 class PadToSquare:
